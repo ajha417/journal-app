4362d8d3e9cf050397cc0eb02abebb4935ba7ee0
ajha417 <ajha417@rku.ac.in>
2025-09-01 23:02:29 +0530

    code changes in userrepo impl for providing proper filtered user based on some criteria

--- src/main/java/com/app/journalapp/repo/UserRepoImpl.java
+++ src/main/java/com/app/journalapp/repo/UserRepoImpl.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -25,7 +26,13 @@ public class UserRepoImpl {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Users> criteriaQuery  = criteriaBuilder.createQuery(Users.class);
         Root<Users> user = criteriaQuery.from(Users.class);
-        criteriaQuery.select(user).where(criteriaBuilder.isTrue(user.get("sentimentAnalysis")));
+        Predicate hasSentimentAnalysis = criteriaBuilder.isTrue(user.get("sentimentAnalysis"));
+        var emailPath = user.get("email").as(String.class);
+        Predicate hasEmail = criteriaBuilder.isNotNull(emailPath);
+        Predicate emailNonEmpty = criteriaBuilder.greaterThan(criteriaBuilder.length(emailPath), 0);
+        Predicate validEmail = criteriaBuilder.like(criteriaBuilder.lower(emailPath), "%_@_%._%");
+        criteriaQuery.select(user).where(criteriaBuilder.and(hasSentimentAnalysis, hasEmail, emailNonEmpty, validEmail));
+//        criteriaQuery.select(user).where(criteriaBuilder.isTrue(user.get("sentimentAnalysis")));
         return entityManager.createQuery(criteriaQuery).getResultList();
     }
 }
---
+++ src/main/java/com/app/journalapp/scheduler/UsersScheduler.java
@@ -0,0 +1,5 @@
+package com.app.journalapp.scheduler;
+
+public class UsersScheduler {
+
+}
