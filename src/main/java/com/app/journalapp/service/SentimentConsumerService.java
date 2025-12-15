4fd4c1ed2fd4b5489490eeee5c4a289f8f54506d
ajha417 <ajha417@rku.ac.in>
2025-12-15 21:32:05 +0530

    adds code to send data via kafka producer and receive it through consumer

---
+++ src/main/java/com/app/journalapp/service/SentimentConsumerService.java
@@ -0,0 +1,22 @@
+package com.app.journalapp.service;
+
+import com.app.journalapp.model.SentimentData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SentimentConsumerService {
+
+    @Autowired
+    private EmailService emailService;
+
+    @KafkaListener(topics = "weekly-sentiment-analysis", groupId = "group_id")
+    public void consume(SentimentData sentimentData) {
+        sendEmail(sentimentData);
+    }
+
+    private void sendEmail(SentimentData sentimentData) {
+        emailService.sendEmail(sentimentData.getEmail(), "Weekly Sentiment Analysis", sentimentData.getSentiment());
+    }
+}
