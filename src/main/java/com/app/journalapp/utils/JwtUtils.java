44e97af1d2bcd5cc6338fd8ab6cec0586e9d3c85
ajha417 <ajha417@rku.ac.in>
2025-12-28 09:36:11 +0530

    Integrates JWT authentication in the application.

---
+++ src/main/java/com/app/journalapp/utils/JwtUtils.java
@@ -0,0 +1,38 @@
+package com.app.journalapp.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtils {
+
+    private static final String SECRET_KEY = "TAk@#$@#$laskjfac.^^$^aZ8VV";
+
+    public String generateJwtToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().
+                claims(claims)
+                .subject(subject)
+                .header().empty().add("typ", "JWT")
+                .and()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5)) // 5 min
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+}
